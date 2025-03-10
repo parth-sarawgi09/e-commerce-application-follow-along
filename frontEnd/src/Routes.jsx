a7908d43196b6aa8929f68c61f33c9844cd7a14e
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-03-10 11:02:32 +0530

    milestone 23

--- frontEnd/src/Routes.jsx
+++ frontEnd/src/Routes.jsx
@@ -7,4 +7,5 @@ import Cart from "./Pages/cart.jsx";
 import ProductDetails from "./Pages/productDetails.jsx";
 import Profile from "./Pages/profile.jsx";
 import CreateAddress from "./Pages/createAddress.jsx";
-export {LoginPage,SignupPage,Home,CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress};
+import SelectAddress from "./Pages/SelectAddress.jsx";
+export {LoginPage,SignupPage,Home,CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress};
